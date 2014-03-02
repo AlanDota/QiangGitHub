c12ab5ebdf584e6c5998c1ad5f2c9c014ea5950a
AlanDota <944341387@qq.com>
2014-03-02 09:59:05 +0800

    version 1.0
    Make a FiletoArray.cs file to have a test first .

---
+++ FileToArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThirdTask {
+	class FileToArray {
+		int col;
+	    string fileUrl;
+		public int Col {
+			get { return col; }
+			set { col = value; }
+		}
+		public string FileUrl { get { return fileUrl; }
+			set { this.fileUrl = value; }
+		}
+		
+		public string ReadFile(string fileUrl) 
+		{
+			StreamReader file = new StreamReader(fileUrl);
+			string fileString = file.ReadToEnd();
+			return fileString;
+		}
+		public string[] FileSeprator(string fileString) 
+		{
+			char[] seprator = new char[] { ',', '\n' };
+			string[] fileSeprate = fileString.Split(seprator);
+			return fileSeprate;
+		
+		}
+		public int Colnum(string fileUrl) 
+		{
+			StreamReader file = new StreamReader(fileUrl);
+			string firstRow = file.ReadLine();
+			char[] seprator = new char[] { ',' };
+			string[] firstRowSeprate = firstRow.Split(seprator);
+			return firstRowSeprate.Length;
+		
+		}
+
+	}
+}
